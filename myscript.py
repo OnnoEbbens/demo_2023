68e31411d19aef6df09bb5bc4d0bc631e63689ba
OnnoEbbens <onnoebbens@gmail.com>
2023-08-17 08:56:50 +0200

    removed some unnecessary code

--- myscript.py
+++ myscript.py
@@ -4,8 +4,6 @@ today = dt.date.today()
 
 # check the birthday
 if today.month == 8 and today.day == 31:
-    print('it is Omar his birthday')
+    print("it is Omar his birthday")
 
 print(today)
-
-print('adasdfasdditionalyyy')
